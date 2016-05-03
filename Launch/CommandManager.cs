2309e9d844c7e31bd2324cd0da566e89428e2498
Ozawa Masayuki <ozawa@coroq.com>
2016-05-03 18:50:45 +0900

    コマンド履歴から検索

--- Launch/CommandManager.cs
+++ Launch/CommandManager.cs
@@ -15,10 +15,13 @@ namespace Launch
     class CommandManager
     {
         private List<Command> commands;
+        private List<Command> History;
+        const int HistorySize = 10;
 
         public CommandManager()
         {
             commands = new List<Command>();
+            History = new List<Command>();
             Environment.SpecialFolder[] menus = {
                 Environment.SpecialFolder.StartMenu,
                 Environment.SpecialFolder.CommonStartMenu
@@ -35,9 +38,24 @@ namespace Launch
             {
                 return new Command[] {};
             }
+            var found0 = FindHistory(query);
             var found1 = FindWithSubstring(query);
             var found2 = FindWithRegex(query);
-            return found1.Concat(found2).Distinct().ToArray();
+            return found0.Concat(found1).Concat(found2).Distinct().ToArray();
+        }
+
+        private List<Command> FindHistory(string query)
+        {
+            var found = new List<Command>();
+            for (var i = 0; i < History.Count; ++i)
+            {
+                var command = History[i];
+                if (command.Name.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    found.Add(command);
+                }
+            }
+            return found;
         }
 
         private List<Command> FindWithSubstring(string query)
@@ -76,6 +94,12 @@ namespace Launch
 
         public void Launch(Command command)
         {
+            History.Remove(command);
+            History.Add(command);
+            if (History.Count > HistorySize)
+            {
+                History = History.GetRange(1, HistorySize);
+            }
             System.Diagnostics.Process.Start(command.Path);
         }
 
