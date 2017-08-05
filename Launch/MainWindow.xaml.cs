451df2df6563880d67a2a95341ccd8de0cb3a1c6
Ozawa Masayuki <ozawa@coroq.com>
2017-08-05 09:38:13 +0900

    Window 表示時にフォーカスを取得

--- Launch/MainWindow.xaml.cs
+++ Launch/MainWindow.xaml.cs
@@ -26,6 +26,7 @@ namespace Launch
                 {
                     commandWindow.Show();
                     commandWindow.Activate();
+                    commandWindow.Focus();
                 }
             };
         }
